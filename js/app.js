f8b8f462f697485de002a14ab79f8e720836d60e
margarita <margarita@intervolga.ru>
2017-06-30 18:17:03 +0300

    added clouds, change background image

--- js/app.js
+++ js/app.js
@@ -1,30 +1,33 @@
+import 'babel-polyfill';
 import {parallax} from 'parallax';
+import $ from 'jquery';
+ import {lettering} from 'lettering';
+import  'ScrollMagic';
+
+window.onload = function(){
+
+  var scene = document.getElementById('scene');
+  if(scene) {
+    var action = new Parallax(scene, {
+      relativeInput: true,
+      clipRelativeInput: false,
+      hoverOnly: false,
+      calibrateX: false,
+      calibrateY: true,
+      invertX: true,
+      invertY: true,
+      limitX: false,
+      limitY: 900,
+      scalarX: 40,
+      scalarY: 20,
+      frictionX: 0.8,
+      frictionY: 0.2,
+      originX: 0.5,
+      originY: 1.0,
+      precision: 1,
+      pointerEvents: false,
+    });
+  }
 
 
-
-var scene = document.getElementById('scene');
-var action = new Parallax(scene, {
-  relativeInput: true,
-  clipRelativeInput: false,
-  hoverOnly: false,
-  calibrateX: false,
-  calibrateY: true,
-  invertX: true,
-  invertY: true,
-  limitX: false,
-  limitY: 500,
-  scalarX: 40,
-  scalarY: 10,
-  frictionX: 0.8,
-  frictionY: 0.2,
-  originX: 0.5,
-  originY: 1.0,
-  precision: 1,
-  pointerEvents: false,
-  onReady: function() { alert('ready!'); }
-});
-
-// action.enable();
-
-
-console.log('kek20')
+}
